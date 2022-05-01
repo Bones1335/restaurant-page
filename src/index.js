3a15905b205642c660d82b51ef9d2d45e62fbfd5
Alexander <ahansen1335@gmail.com>
2022-05-01 10:14:22 +0200

    add stylesheet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './style.css';
+import Icon from './images/background.jpg';
+
 console.log('webpack working');
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,19 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map', // remove when finished
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
 };
