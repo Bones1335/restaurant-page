042e551ca65966d8d1ef219f216635cdfb6b3d10
Alexander <ahansen1335@gmail.com>
2022-05-01 08:58:06 +0200

    change gitignore settings for personal understanding of what is happening

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('webpack working');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLCtCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd3ZWJwYWNrIHdvcmtpbmcnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
